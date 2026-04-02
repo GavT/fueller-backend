46488f14a98fe976e2fe39cf8df2179abbb8394c
Gavin Thomas <24224467+GavT@users.noreply.github.com>
2026-04-02 21:55:39 +0100

    Update shadow plugin version to 8.3.6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.serialization") version "1.9.24"
     application
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.johnrengelman.shadow") version "8.3.6"
 }
 
 group = "uk.co.fueller"
